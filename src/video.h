96dbec8f1ead466c193f521cac846cacbc31b7b3
Carlos C <carlitoscarreong@hotmail.com>
2025-06-07 13:03:35 -0600

    added the class Video and its attributes

---
+++ src/video.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <string>
+
+using std::string;
+
+class Video {
+private:
+    int id;
+    string nombre;
+    float duracion;
+    string genero;
+    int calificacion;
+};
