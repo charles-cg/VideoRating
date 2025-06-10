c793de37e01eafaa97a151eebc48f51f8e384693
MATEO HERNANDEZ <mateohernandez@MacBook-Air-de-MATEO-2.local>
2025-06-10 14:47:01 -0600

    Se agregó el switch de loadPeliculas

--- src/datos.h
+++ src/datos.h
@@ -11,7 +11,10 @@ public:
     int readPeliculaLines(std::string filename);
     bool loadEpisodioCSV(std::string filename, unsigned int arraySize);
     int readEpisodioLines(std::string filename);
+    bool loadVideosCSV(std::string filename, unsigned int arraySize);
+    int readVideosLines(std::string filename);
 private:
     Episodio *episodioArray;
     Pelicula *peliculaArray;
+    Video *videoArray;
 };
--- src/pelicula.cpp
+++ src/pelicula.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include "pelicula.h"
+#include <fstream>
+#include <sstream>
 
 using namespace std;
 
@@ -18,4 +20,53 @@ void Pelicula::printData() {
 	cout << "Duracion: " << getDuracion();
 	cout << "Genero: " << getGenero();
     cout << "Calificacion: " << getCalificacion();
+}
+
+bool loadPeliculas(string filename, unsigned int arraySIze) {
+	ifstream file(filename);
+	string line;
+	unsigned int size = 0;
+
+	if (!file.is_open()) {
+		cout << "No se pudo abrir el archivo: " << filename <<endl;
+		return false;
+	}
+	if (!getline (file, line)) {
+		cout << "El archivo no tiene header" << endl;
+		return false;
+	}
+	cout << "Archivo abierto"<<endl;
+	//Aquí preguntar a Artemio si copiamos parte por parte el código
+	while (getline (file, line)) {
+		Pelicula newPeli;
+		stringstream ss(line);
+		string cell;
+		int campo = 0, errores = 0;
+
+		while (getline(ss, cell, ',')) {
+			if (!cell.length())
+				errores++;
+			switch (campo){
+				case 0:
+					newPeli.setId(cell);
+					break;
+				case 1:
+					newPeli.setNombre(cell);
+					break;
+				case 2:
+					newPeli.setCalificacion(stod(cell));
+					break;
+				case 3:
+					newPeli.setGenero(cell);
+					break;
+				case 4:
+					newPeli.setDuracion();
+				default:
+					errores++;
+					break;
+				}
+			}
+		}
+
+	}
 }
